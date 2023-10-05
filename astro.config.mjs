523f4e48a4a45835b8545aa225801717113d9dd6
Sille <cekj0002@stud.kea.dk>
2023-10-05 10:30:17 +0200

    Merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // Example: astro.config.mjs
-export default {
-  // ... other config settings
-  routes: [
-    {
-      path: "/", // Root path
-      component: "src/pages/index.astro", // Example: Use an existing component for the root page
-    },
-    {
-      path: "/liste", // URL path for the new page
-      component: "src/pages/liste.astro", // Use the component you created
-    },
-    // ... other routes
-  ],
-};
+export default defineConfig({});
